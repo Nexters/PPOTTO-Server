132c6c7d6ea43319895461db595501d35c2f4ce7
Son Daehyeon <sondaehyeon01@gmail.com>
2026-07-25 14:50:49 +0900

    feat(config): CorsProperties 검증 추가

--- src/main/kotlin/com/github/nexters/ppotto/global/config/CorsProperties.kt
+++ src/main/kotlin/com/github/nexters/ppotto/global/config/CorsProperties.kt
@@ -1,8 +1,12 @@
 package com.github.nexters.ppotto.global.config
 
+import jakarta.validation.constraints.NotEmpty
 import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.validation.annotation.Validated
 
+@Validated
 @ConfigurationProperties(prefix = "cors")
 data class CorsProperties(
-    val allowedOrigins: List<String>,
+    @field:NotEmpty
+    val allowedOrigins: List<@NotEmpty String>,
 )
