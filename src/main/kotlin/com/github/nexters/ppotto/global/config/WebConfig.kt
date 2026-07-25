a18bdea069a6b73bd0d3cfc5ec210d5e4e163ff9
Son Daehyeon <sondaehyeon01@gmail.com>
2026-07-25 14:08:01 +0900

    feat(config): CORS 설정 추가

---
+++ src/main/kotlin/com/github/nexters/ppotto/global/config/WebConfig.kt
@@ -0,0 +1,21 @@
+package com.github.nexters.ppotto.global.config
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig(
+    @Value("\${cors.allowed-origins}") private val allowedOrigins: List<String>,
+) : WebMvcConfigurer {
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins(*allowedOrigins.toTypedArray())
+            .allowedMethods("*")
+            .allowedHeaders("*")
+            .allowCredentials(true)
+            .maxAge(3600)
+    }
+}
