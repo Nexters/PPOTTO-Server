ad1211559b76091327cbb37c0f42b6c7b89be372
Son Daehyeon <sondaehyeon01@gmail.com>
2026-07-25 14:23:23 +0900

    refactor(test): BehaviorSpec 기반 Given-When-Then 패턴으로 전환

--- src/test/kotlin/com/github/nexters/ppotto/ApplicationIntegrationTest.kt
+++ src/test/kotlin/com/github/nexters/ppotto/ApplicationIntegrationTest.kt
@@ -7,7 +7,11 @@ import org.jooq.DSLContext
 class ApplicationIntegrationTest(
     dsl: DSLContext,
 ) : IntegrationTest({
-    test("애플리케이션 컨텍스트가 로드되고 데이터베이스에 연결된다") {
-        dsl.selectOne().fetchSingle().value1() shouldBe 1
+    Given("애플리케이션이 기동된 상태에서") {
+        When("데이터베이스에 쿼리를 실행하면") {
+            Then("정상적으로 응답한다") {
+                dsl.selectOne().fetchSingle().value1() shouldBe 1
+            }
+        }
     }
 })
--- src/test/kotlin/com/github/nexters/ppotto/support/IntegrationTest.kt
+++ src/test/kotlin/com/github/nexters/ppotto/support/IntegrationTest.kt
@@ -1,9 +1,9 @@
 package com.github.nexters.ppotto.support
 
-import io.kotest.core.spec.style.FunSpec
+import io.kotest.core.spec.style.BehaviorSpec
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.context.annotation.Import
 
 @SpringBootTest
 @Import(TestcontainersConfiguration::class)
-abstract class IntegrationTest(body: FunSpec.() -> Unit = {}) : FunSpec(body)
+abstract class IntegrationTest(body: BehaviorSpec.() -> Unit = {}) : BehaviorSpec(body)
