6cd2ccad7d35d1af99b2d4f8c94eae4ee022705c
Son Daehyeon <sondaehyeon01@gmail.com>
2026-07-25 14:11:11 +0900

    refactor(config): CORS 설정을 ConfigurationProperties로 전환

--- src/main/kotlin/com/github/nexters/ppotto/PpottoApplication.kt
+++ src/main/kotlin/com/github/nexters/ppotto/PpottoApplication.kt
@@ -1,9 +1,11 @@
 package com.github.nexters.ppotto
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 class PpottoApplication
 
 fun main(args: Array<String>) {
--- src/main/kotlin/com/github/nexters/ppotto/global/config/WebConfig.kt
+++ src/main/kotlin/com/github/nexters/ppotto/global/config/WebConfig.kt
@@ -1,18 +1,17 @@
 package com.github.nexters.ppotto.global.config
 
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 class WebConfig(
-    @Value("\${cors.allowed-origins}") private val allowedOrigins: List<String>,
+    private val corsProperties: CorsProperties,
 ) : WebMvcConfigurer {
 
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
-            .allowedOrigins(*allowedOrigins.toTypedArray())
+            .allowedOrigins(*corsProperties.allowedOrigins.toTypedArray())
             .allowedMethods("*")
             .allowedHeaders("*")
             .allowCredentials(true)
