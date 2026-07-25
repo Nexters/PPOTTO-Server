edd100f0009a29c8652f4c460fd1dce5d7fed568
Son Daehyeon <sondaehyeon01@gmail.com>
2026-07-25 14:50:49 +0900

    refactor(config): placeholder 기본값을 env 템플릿으로 이관

--- src/test/kotlin/com/github/nexters/ppotto/support/IntegrationTest.kt
+++ src/test/kotlin/com/github/nexters/ppotto/support/IntegrationTest.kt
@@ -3,8 +3,10 @@ package com.github.nexters.ppotto.support
 import io.kotest.core.spec.style.BehaviorSpec
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.context.annotation.Import
+import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
+@ActiveProfiles("test")
 @Import(TestcontainersConfiguration::class)
 abstract class IntegrationTest(
     body: BehaviorSpec.() -> Unit = {},
